613edc1bd8c2f6cb8ecf8d344e830691708a75ce
Success Kingsley <successxodev@gmail.com>
2025-03-10 17:15:12 -0400

    chore: Initial project setup with Nix flake, CI workflow, and licensing

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Fast file search tool that understands content.";
+
+  inputs = {
+    nixpkgs.url =
+      "github:NixOS/nixpkgs?rev=a47b881e04af1dd6d414618846407b2d6c759380";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        manifest = pkgs.lib.importTOML ./Cargo.toml;
+        package = manifest.package;
+        snapfind = pkgs.rustPlatform.buildRustPackage {
+          pname = package.name;
+          version = package.version;
+          src = pkgs.lib.cleanSource ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          meta = with pkgs.lib; {
+            inherit (package) description homepage repository;
+            license = licenses.mit;
+            maintainers = [ maintainers.xosnrdev ];
+          };
+        };
+
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.cargo-watch
+            pkgs.cargo-sort
+            pkgs.git-cliff
+            pkgs.cargo-release
+            pkgs.cargo-dist
+            pkgs.git
+            pkgs.cargo-bloat
+          ];
+          shellHook = ''
+            export RUST_BACKTRACE=1
+          '';
+        };
+
+      in {
+        formatter = pkgs.nixfmt-classic;
+        packages = { default = snapfind; };
+        devShells.default = devShell;
+      });
+}
